55e152d1950f561e4866ef8bf048eff9bce60738
David Dubois <ddubois@metrocast.net>
2017-11-17 13:48:31 -0500

    changed production.rb to run in development mode by adding line 22 in file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,8 @@ Rails.application.configure do
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
-
+  #A sneaky trick to "debug" on Heroku is to actually make your app run in "development" mode. Although you must ensure you switch it back to running in production mode afterwards, if you do the following, it will show you what the error is properly:
+config.consider_all_requests_local = true # false
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
